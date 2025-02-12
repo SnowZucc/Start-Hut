69e45dc9ea4a155d5c38b7272d1f98f3da85e58f
BenamarMeriemGit <meryemb16b@gmail.com>
2025-02-12 23:39:33 +0100

    Finalisation html de la 1ere section de la landing page avec le css( pas final)

--- index.php
+++ index.php
@@ -11,12 +11,19 @@
         <?php include('header.php'); ?>             <!-- Rajoute le header par la magie de PHP  -->
         
         <div class="content">                       <!-- on mets tout dans cette classe pour que les info soient centré -->
-            <div class=" landing">
-                 <h1>START-HUT</h1>                    <!-- Titre 1 : start hut de la landing page -->
-                <p>L’outil le plus simple <br>         <!-- ici le br permet de sauter les lignes dans le paragraphe -->
-                pour transformer vos idées en startups.</p>
-                <button class="btn">EN SAVOIR PLUS</button>      <!-- Bouton en savoir plus qui enverra vers une page qu'on fera plus 
-                                                tard ou yaura des info sur lentreprise, prq start-hut et comment sa marche  -->
+            <div class="landing">
+                <div class="landing-text">
+                    <h1>START-HUT</h1>                <!-- Titre 1 : start hut de la landing page -->
+                    <p>
+                        L’outil le plus simple <br>   <!-- ici le br permet de sauter les lignes dans le paragraphe -->
+                        pour <span class="highlight">transformer</span> vos idées en startups. <!-- permet de différencier le mot transformer du reste du texte -->
+                    </p>      
+                    <button class="btn">EN SAVOIR PLUS</button>      <!-- Bouton en savoir plus qui enverra vers une page qu'on fera plus
+                                                    tard ou yaura des info sur lentreprise, prq start-hut et comment sa marche  -->
+                </div>
+                <div class="landing-image">   <!-- image temporaire si vous avez des idées de image send it to me-->
+                    <img src="landing.png" alt="Illustration">
+                </div>
             </div>
         </div>
     </body>
