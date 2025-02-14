e220e8b84eef5282c1c1e0eceeb964aaeee743d7
guillaume-fvt <150684460+guillaume-fvt@users.noreply.github.com>
2025-02-14 11:59:55 +0100

    Création de la base de l'inscription (info texte)

---
+++ inscription.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+        <?php include('header.php'); ?>   
+    </head>
+
+    <body>
+        <h1>Inscription</h1>
+        <p> Déjà inscrit ? <a href="connexion.php">Connectez-vous</a></p> <!--  ATTENTION Modifier le lien de connexion lors de la création de la page de connexion -->
+        <h3>Adresse Mail</h3>
+        <h3>Mot de passe</h3>
+        <h3>Confirmation du mot de passe</h3>
+
+    <?php include('footer.php'); ?>    
+    </body>
+</html>
