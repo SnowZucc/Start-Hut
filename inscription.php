3eabe1df3aaeb4f6b67469ac482f296d6cf58b60
guillaume-fvt <150684460+guillaume-fvt@users.noreply.github.com>
2025-02-22 11:50:44 +0100

    Ajout des styles CSS

--- inscription.php
+++ inscription.php
@@ -3,21 +3,40 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="styles.css?v=3">
         <?php include('header.php'); ?>   
     </head>
 
     <body>
-        <h1>Inscription</h1>
-        <p> Déjà inscrit ? <a href="connexion.php">Connectez-vous</a></p> <!--  ATTENTION Modifier le lien de connexion lors de la création de la page de connexion -->
-        <h3>Adresse Mail</h3>
-            <input type="text" class="search-bar" placeholder="Veuillez saisir votre adresse mail">
-        <h3>Mot de passe</h3>
-            <input type="text" class="search-bar" placeholder="Veuillez saisir votre mot de passe">
-        <h3>Confirmation du mot de passe</h3>
-            <input type="text" class="search-bar" placeholder="Veuillez saisir votre mot de passe">
+        <h1 class="inscription" >INSCRIPTION</h1>
+        <p class="already-registered"> Déjà inscrit ? <a href="connexion.php">Se Connecter</a></p> <!--  ATTENTION Modifier le lien de connexion lors de la création de la page de connexion -->
+
+        <form action= "inscription.php" method="POST" class="inscription">
+            <div class="form-group">
+                <label for="email">Adresse mail<span class="required"></span></label>
+                <input type="email" name= "email" class="input-field" placeholder="Veuillez saisir votre adresse mail" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Mot de passe <span class="required">*</span></label>
+                <input type="password" name= "password" class="input-field" placeholder="Veuillez saisir votre mot de passe" required>
+            </div>
+            
+            <div class="form-group">
+                <label for="password_confirmation">Confirmer votre mot de passe <span class="required">*</sapn></label>
+                <input type="password" name= "password_confirmation" class="input-field" placeholder="Veuillez saisir votre mot de passe" required>
+            </div>
+
+            <div class="checkbox-container">
+                <input type="checkbox" id="cgu" required>
+                <label for="cgu">J'ai lu et j'accepte les <a href="#"><!-- Ajouter les conditons générales d'utilisation -->
+            </div>
+
+            <div class="button-container">
+                <button type="submit" class="btnInscription">S'inscrire</button>
+            </div>
+        </form>
 
     <?php include('footer.php'); ?>    
     </body>
