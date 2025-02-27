18cba41686fd048af8072c249062da0586a09070
Battl3tague <quentin.eustache@gmail.com>
2025-02-27 13:47:43 +0100

    Ajout de photo plus modif de 2/3 boxs

--- profil.php
+++ profil.php
@@ -11,6 +11,14 @@
   <body>
     <?php include('header.php'); ?>         <!-- Rajoute le header par la magie de PHP  -->
       <div class="content">
+            <div class="gride">
+              <figure>
+                <img src="https://wallsdesk.com/wp-content/uploads/2017/01/Mark-Zuckerberg-Wallpapers.jpg">
+                <figcaption>
+                  <h3>Photo de profil </h3>
+                </figcaption>
+              </figure>
+            </div>
          <div class="name">
              <input type="text" class="nom" placeholder="Votre nom">
          </div> 
@@ -48,7 +56,11 @@
               <option value="saab">Russie</option>
             </select>
           </div>
-        
+          <form method="post" enctype="multipart/form-data">
+            <label for="file-upload" class="CV CV-label">Sélectionner un fichier</label>
+            <input type="file" class="CV-input" id="file-upload" name="fichier">
+            <button type="submit" class="CV-button">Envoyer</button>
+          </form>
         </div>
            
     </div> 
