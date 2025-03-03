e02532d6cd66d7da73641d5b04e0270c15890af8
Zucc <louis.alexandre@eleve.isep.fr>
2025-03-03 12:22:12 +0100

    Correction HTML et CSS pour l'index

--- index.php
+++ index.php
@@ -11,79 +11,77 @@
     <body>
         <?php include('header.php'); ?>             <!-- Rajoute le header par la magie de PHP  -->
         
-             <div class="content">                       <!-- on mets tout dans cette classe pour que les info soient centré -->
-                <div class="landing"> 
-                     <div class="landing-text">
-                        <h1>START-HUT</h1>                <!-- Titre 1 : start hut de la landing page -->
-                        <p>
-                        L’outil le plus simple <br>   <!-- ici le br permet de sauter les lignes dans le paragraphe -->
-                        pour <span class="highlight">transformer</span> vos idées en startups. <!-- permet de différencier le mot transformer du reste du texte -->
-                        </p>      
-                        <button class="btnlanding">EN SAVOIR PLUS</button>      <!-- Bouton en savoir plus qui enverra vers une page qu'on fera plus
-                                                    tard ou yaura des info sur lentreprise, prq start-hut et comment sa marche  -->
-                    </div>
-                    <div class="landing-image">   <!-- image temporaire si vous avez des idées de image send it to me-->
-                        <img src="src/img/landing.png" alt="Illustration">
-                    </div>
-                </div>  
-                 <!-- partie annonce populaire -->
-                <div class ="textepop">  <!-- texte --> 
+        <div class="content">                       <!-- on mets tout dans cette classe pour que les info soient centré -->
+            <div class="landing"> 
+                <div class="landing-text">
+                    <h1>START-HUT</h1>                <!-- Titre 1 : start hut de la landing page -->
+                    <p>
+                    L’outil le plus simple <br>   <!-- ici le br permet de sauter les lignes dans le paragraphe -->
+                    pour <span class="highlight">transformer</span> vos idées en startups. <!-- permet de différencier le mot transformer du reste du texte -->
+                    </p>      
+                    <button class="btnlanding">EN SAVOIR PLUS</button>      <!-- Bouton en savoir plus qui enverra vers une page qu'on fera plus
+                                                tard ou yaura des info sur lentreprise, prq start-hut et comment sa marche  -->
+                </div>
+                <div class="landing-image">   <!-- image temporaire si vous avez des idées de image send it to me-->
+                    <img src="src/img/landing.png" alt="Illustration">
+                </div>
+            </div>  
+            <!-- partie annonce populaire -->
+            <div class ="textepop">  <!-- texte --> 
                 <h2>Nos annonces<br>
                 <span class ="highlight"> populaires</span>  <!-- pour stylisée le mot populaire uniquement -->
                 </h2> 
+            </div>
+            <div class ="popular">  <!-- les annonces (5 annonces pour le moment)-->
+                <figure class = "popular-annonce">
+                <h3>popular 1</h3>
+                </figure>
+                <figure class = "popular-annonce">
+                <h3>popular 2</h3>
+                </figure>
+                <figure class = "popular-annonce">
+                <h3>popular 3</h3>
+                </figure>
+                <figure class = "popular-annonce">
+                <h3>popular 4</h3>
+                </figure>
+                <figure class = "popular-annonce">
+                <h3>popular 5</h3>
+                </figure>
+            </div>
+            <!-- Partie pourquoi nous chosir -->
+            <div class="conteneur-pourquoinouschoisir">
+                <div class="imagepourquoinouschoisir">
+                    <img src="src/img/pourquoinouschoisir.png" alt="illustration">
                 </div>
-                <div class ="popular">  <!-- les annonces (5 annonces pour le moment)-->
-                    <figure class = "popular-annonce">
-                    <h3>popular 1</h3>
-                    </figure>
-                    <figure class = "popular-annonce">
-                    <h3>popular 2</h3>
-                    </figure>
-                    <figure class = "popular-annonce">
-                    <h3>popular 3</h3>
-                    </figure>
-                    <figure class = "popular-annonce">
-                    <h3>popular 4</h3>
-                    </figure>
-                    <figure class = "popular-annonce">
-                    <h3>popular 5</h3>
-                    </figure>
+                <div class="textepourquoinouschoisir">
+                    <h2>Lancez vous, c'est facile.</h2>
+                    <div class="liste-choix">
+                        <figure>
+                            <img src="src/img/pourquoinouschoisir1.png" alt="Image 1">
+                            <figcaption>
+                                <h3>RAISON 1</h3>
+                                <p>Description </p>
+                            </figcaption>
+                        </figure>
+                        <figure>
+                            <img src="src/img/pourquoinouschoisir2.png" alt="Image 2">
+                            <figcaption>
+                                <h3>RAISON 2</h3>
+                                <p>Description </p>
+                            </figcaption>
+                        </figure>
+                        <figure>
+                            <img src="src/img/pourquoinouschosir3.png" alt="Image 3">
+                            <figcaption>
+                                <h3>RAISON 3</h3>
+                                <p>Description </p>
+                            </figcaption>
+                        </figure>
+                    </div>
                 </div>
             </div>
-           <!-- Partie pourquoi nous chosir -->
-           <div class="conteneur-pourquoinouschoisir">
-    <div class="imagepourquoinouschoisir">
-        <img src="src/img/pourquoinouschoisir.png" alt="illustration">
-    </div>
-    <div class="textepourquoinouschoisir">
-        <h2>Lancez vous, c'est facile.</h2>
-        <div class="liste-choix">
-            <figure>
-                <img src="src/img/pourquoinouschoisir1.png" alt="Image 1">
-                <figcaption>
-                    <h3>RAISON 1</h3>
-                    <p>Description </p>
-                </figcaption>
-            </figure>
-            <figure>
-                <img src="src/img/pourquoinouschoisir2.png" alt="Image 2">
-                <figcaption>
-                    <h3>RAISON 2</h3>
-                    <p>Description </p>
-                </figcaption>
-            </figure>
-            <figure>
-                <img src="src/img/pourquoinouschosir3.png" alt="Image 3">
-                <figcaption>
-                    <h3>RAISON 3</h3>
-                    <p>Description </p>
-                </figcaption>
-            </figure>
         </div>
-    </div>
-</div>
-
-
 
         <?php include('footer.php'); ?>    
     </body>
