ca8808f3db5cef18200c6c48b8cc5c6f08a5909e
Khaleb0 <dariustchoumbou@gmail.com>
2025-03-04 00:25:14 +0100

    ajout de espace projets dans le header

--- header.php
+++ header.php
@@ -8,12 +8,13 @@
             <li><a href="posterannonce.php">Poster une annonce</a></li>
             <li><a href="abonnement.php">Abonnement</a></li>
             <li><a href="FAQ.php">FAQ</a></li>
+            <li><a href="monannonce.php">Espace Projets</a></li>
         </ul>
         <div class="auth-buttons"> 
             <a href="connexion.php" class="login">Connexion</a>
             <a href="inscription.php" class="signup" >Inscription</a>
             <a href="profil.php" class="signup">Mon profil</a>
-            <a href="monannonce.php" class="signup">Mes annonces</a>
+           
         </div>
     </nav>
 
