0ff61ec48673f8cf69260bf405a48c7e7f2993a1
Battl3tague <quentin.eustache@gmail.com>
2025-05-02 12:01:09 +0200

    ajout de description et Langues dans la db avec le changement de statut (à finir)

--- database/database.sql
+++ database/database.sql
@@ -7,6 +7,8 @@ CREATE TABLE Utilisateurs (
     prenom VARCHAR(255) NOT NULL ,
     email VARCHAR(255) NOT NULL UNIQUE,
     mot_de_passe VARCHAR(255) NOT NULL,
+    description_profil VARCHAR(255) NOT NULL,
+    langues_parlees VARCHAR(255) NOT NULL,
     type ENUM('porteur', 'collaborateur') NOT NULL
 );
 
